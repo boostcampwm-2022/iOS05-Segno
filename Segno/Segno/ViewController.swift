2f36851bdfda19800cb309848400d6138d2134a2
Gordon Choi <thefirate@gmail.com>
2022-11-09 22:37:09 +0900

    close #1 chore: 프로젝트 생성

---
+++ Segno/Segno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
