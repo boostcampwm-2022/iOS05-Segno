1dd9b148b7c6ee8799a836c8bdaf2462ee557373
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2022-11-10 18:09:08 +0900

    #6 feat: SceneDelegate -> AppCoordinator -> LoginCoordinator -> vc flow 구성

---
+++ Segno/Segno/Presentation/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  LoginCoordinator.swift
+//  Segno
+//
+//  Created by 이예준 on 2022/11/10.
+//
+
+import UIKit
+
+final class LoginCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController()
+        self.navigationController.pushViewController(vc, animated: true)
+    }
+}
