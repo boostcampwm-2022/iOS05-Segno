829296ef4347be52ed7f0993159a8a16bb72d321
Gordon Choi <thefirate@gmail.com>
2022-11-10 18:32:54 +0900

    close #8 feat: DiaryDetail에 음악과 위치 정보 추가

--- Segno/Segno/Entity/DiaryDetail.swift
+++ Segno/Segno/Entity/DiaryDetail.swift
@@ -5,12 +5,14 @@
 //  Created by Gordon Choi on 2022/11/10.
 //
 
+import CoreLocation
+
 struct DiaryDetail {
     let id: String
     let title: String
     let tags: [String]
     let imagePath: String
     let bodyText: String?
-    // 음악 정보
-    // 위치 정보
+    let musicInfo: MusicInfo?
+    let location: CLLocation?
 }
