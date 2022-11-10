d23a81f8042da2503787c87665b6f0d9298bcf7b
Gordon Choi <thefirate@gmail.com>
2022-11-10 18:13:19 +0900

    #8 feat: 일기 상세 화면을 위한 모델 정의

---
+++ Segno/Segno/Entity/DiaryDetail.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryDetail.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/11/10.
+//
+
+struct DiaryDetail {
+    let id: String
+    let title: String
+    let tags: [String]
+    let imagePath: String
+    let bodyText: String?
+    // 음악 정보
+    // 위치 정보
+}
