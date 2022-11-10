603cc13e7b72ed38052e949bb6065ae81df5b0a9
Gordon Choi <thefirate@gmail.com>
2022-11-10 18:03:11 +0900

    close #7 feat: 일기 리스트 상의 개별 항목에 대한 모델 작성

---
+++ Segno/Segno/Entity/DiaryListItem.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryListItem.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/11/10.
+//
+
+struct DiaryListItem: Hashable {
+    let id: String
+    let title: String
+    let thumbnailPath: String
+}
