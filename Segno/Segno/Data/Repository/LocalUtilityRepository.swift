266ca13700a44bd27ad1da9bd451a4f3ca51165e
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-14 17:57:30 +0900

    #16 feat: 중복된 키 값 추가시 실행되는 updateItem 함수 작성

--- Segno/Segno/Data/Repository/LocalUtilityRepository.swift
+++ Segno/Segno/Data/Repository/LocalUtilityRepository.swift
@@ -24,11 +24,32 @@ final class LocalUtilityRepositoryImpl: LocalUtilityRepository {
             let result: Bool = {
                 let status = SecItemAdd(addQuery as CFDictionary, nil)
                 if status == errSecSuccess { return true }
+                else if status == errSecDuplicateItem {
+                    return self.updateItem(value: token, key: key)
+                }
                 return false
             }()
             
             single(.success(result))
             return Disposables.create()
         }
+    }
+    
+    private func updateItem(value: Any, key: Any) -> Bool {
+        let prevQuery: [CFString: Any] = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key
+        ]
+        let updateQuery: [CFString: Any] = [
+            kSecValueData: (value as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any
+        ]
+        
+        let result: Bool = {
+            let status = SecItemUpdate(prevQuery as CFDictionary, updateQuery as CFDictionary)
+            if status == errSecSuccess { return true }
+            return false
+        }()
+        
+        return result
     }
 }
