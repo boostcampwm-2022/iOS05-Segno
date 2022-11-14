50999d183ec87efb8f7d1b72a367150b03bf6378
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-14 17:58:46 +0900

    #16 feat: 민감한 정보를 KeyChain으로부터 불러오는 메서드 작성

--- Segno/Segno/Data/Repository/LocalUtilityRepository.swift
+++ Segno/Segno/Data/Repository/LocalUtilityRepository.swift
@@ -10,7 +10,7 @@ import RxSwift
 
 protocol LocalUtilityRepository {
     func createToken(key: Any, token: Any) -> Single<Bool>
-//    func getToken(key: Any) -> Single<Any>?
+    func getToken(key: Any) -> Single<Any>?
 }
 
 final class LocalUtilityRepositoryImpl: LocalUtilityRepository {
@@ -33,6 +33,28 @@ final class LocalUtilityRepositoryImpl: LocalUtilityRepository {
             single(.success(result))
             return Disposables.create()
         }
+    }
+    
+    func getToken(key: Any) -> Single<Any>? {
+        let getQuery: [CFString: Any] = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnAttributes: true,
+            kSecReturnData: true
+        ]
+        var item: CFTypeRef?
+        let result = SecItemCopyMatching(getQuery as CFDictionary, &item)
+        if result == errSecSuccess {
+            if let existingItem = item as? [String: Any],
+               let data = existingItem[kSecValueData as String] as? Data,
+               let token = String(data: data, encoding: .utf8) {
+                return Single<Any>.create { single in
+                    single(.success(token))
+                    return Disposables.create()
+                }
+            }
+        }
+        return nil
     }
     
     private func updateItem(value: Any, key: Any) -> Bool {
