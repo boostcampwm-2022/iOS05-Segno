1969ed5b2d1fa964f42307751fbf698287ca8b53
BaeRoNuI <xogh20321@gmail.com>
2022-11-14 16:35:39 +0900

    #14 feat: Add extension about Fonts

---
+++ Segno/Segno/Extension/UIFont+.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont+.swift
+//  Segno
+//
+//  Created by TaehoYoo on 2022/11/14.
+//
+
+import UIKit
+
+extension UIFont {
+    static func shiningStar(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Cafe24ShiningStar", size: size)!
+    }
+    
+    static func surround(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Cafe24Ssurround", size: size)!
+    }
+    
+    static func surroundAir(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Cafe24SsurroundAir", size: size)!
+    }
+}
