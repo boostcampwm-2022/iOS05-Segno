7a3a45a9994130ac34aa2662849b3434477f4ce3
Gordon Choi <thefirate@gmail.com>
2022-11-14 16:48:54 +0900

    #13 feat: 네트워크 매니저 정의

---
+++ Segno/Segno/Data/Network/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/11/14.
+//
+
+import Foundation
+import RxSwift
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    private let disposeBag = DisposeBag()
+    
+    private init() {}
+    
+    
+}
