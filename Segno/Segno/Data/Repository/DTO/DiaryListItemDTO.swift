80937f6729863bc7a3c943c8dafe599333f26464
Gordon Choi <thefirate@gmail.com>
2022-11-15 14:52:15 +0900

    #29 feat: 일기 리스트 항목에 대한 DTO 작성

---
+++ Segno/Segno/Data/Repository/DTO/DiaryListItemDTO.swift
@@ -0,0 +1,13 @@
+//
+//  DiaryListItemDTO.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/11/15.
+//
+
+struct DiaryListItemDTO: Decodable {
+    // TODO: 일단은 다이어리 리스트 아이템과 동일하게 작성. 이후 서버 사이드에 따라 바꾸겠습니다.
+    let id: String
+    let title: String
+    let thumbnailPath: String
+}
