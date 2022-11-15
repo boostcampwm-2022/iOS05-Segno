5f697a27be7554bb1a7fcd92f4f4972dc3c0e709
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-15 11:37:52 +0900

    #16 refactor: PR 리뷰 기반하여 메서드 리팩토링 진행

--- Segno/Segno/Data/Repository/LocalUtilityRepository.swift
+++ Segno/Segno/Data/Repository/LocalUtilityRepository.swift
@@ -10,68 +10,73 @@ import RxSwift
 
 protocol LocalUtilityRepository {
     func createToken(key: Any, token: Any) -> Single<Bool>
-    func getToken(key: Any) -> Single<Any>?
+    func getToken(key: Any) -> Single<Any>
 }
 
 final class LocalUtilityRepositoryImpl: LocalUtilityRepository {
     func createToken(key: Any, token: Any) -> Single<Bool> {
-        return Single<Bool>.create { single in
-            let addQuery: [CFString: Any] = [
-                kSecClass: kSecClassGenericPassword,
-                kSecAttrAccount: key,
-                kSecValueData: (token as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any
+        return Single.create { single in
+            let token = (token as AnyObject).data(using: String.Encoding.utf8.rawValue)!
+            let addQuery: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrAccount as String: key,
+                kSecValueData as String: token
             ]
-            let result: Bool = {
-                let status = SecItemAdd(addQuery as CFDictionary, nil)
-                if status == errSecSuccess { return true }
-                else if status == errSecDuplicateItem {
-                    return self.updateItem(value: token, key: key)
-                }
-                return false
-            }()
             
-            single(.success(result))
+            let status = SecItemAdd(addQuery as CFDictionary, nil)
+            if status == errSecSuccess {
+                single(.success(true))
+            }
+            else if status == errSecDuplicateItem {
+                single(.failure(KeychainError.duplicatedKey))
+            }
+            else {
+                single(.failure(KeychainError.unhandledError(status: status)))
+            }
             return Disposables.create()
         }
     }
     
-    func getToken(key: Any) -> Single<Any>? {
-        let getQuery: [CFString: Any] = [
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrAccount: key,
-            kSecReturnAttributes: true,
-            kSecReturnData: true
-        ]
-        var item: CFTypeRef?
-        let result = SecItemCopyMatching(getQuery as CFDictionary, &item)
-        if result == errSecSuccess {
-            if let existingItem = item as? [String: Any],
-               let data = existingItem[kSecValueData as String] as? Data,
-               let token = String(data: data, encoding: .utf8) {
-                return Single<Any>.create { single in
+    func getToken(key: Any) -> Single<Any> {
+        return Single.create { single in
+            let getQuery: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrAccount as String: key,
+                kSecReturnAttributes as String: true,
+                kSecReturnData as String: true
+            ]
+            var item: CFTypeRef?
+            let result = SecItemCopyMatching(getQuery as CFDictionary, &item)
+            if result == errSecSuccess {
+                if let existingItem = item as? [String: Any],
+                   let data = existingItem[kSecValueData as String] as? Data,
+                   let token = String(data: data, encoding: .utf8) {
                     single(.success(token))
-                    return Disposables.create()
+                } else {
+                    single(.failure(KeychainError.unexpectedToken))
                 }
+            } else {
+                single(.failure(KeychainError.unexpectedToken))
             }
+            return Disposables.create()
         }
-        return nil
     }
     
-    private func updateItem(value: Any, key: Any) -> Bool {
-        let prevQuery: [CFString: Any] = [
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrAccount: key
+    func updateToken(key: Any, token: Any) -> Bool {
+        let prevQuery: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
         ]
-        let updateQuery: [CFString: Any] = [
-            kSecValueData: (value as AnyObject).data(using: String.Encoding.utf8.rawValue) as Any
+        let token = (token as AnyObject).data(using: String.Encoding.utf8.rawValue)!
+        let updateQuery: [String: Any] = [
+            kSecValueData as String: token as Any
         ]
         
-        let result: Bool = {
-            let status = SecItemUpdate(prevQuery as CFDictionary, updateQuery as CFDictionary)
-            if status == errSecSuccess { return true }
+        let status = SecItemUpdate(prevQuery as CFDictionary, updateQuery as CFDictionary)
+        if status == errSecSuccess {
+            return true
+        } else {
             return false
-        }()
-        
-        return result
+        }
     }
 }
