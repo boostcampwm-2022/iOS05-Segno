e8b30701b564c16c9891754f58d6fd5b23d88e43
Gordon Choi <thefirate@gmail.com>
2022-11-15 15:40:39 +0900

    #29 feat: 일기 목록을 나타내는 DTO 설정

--- Segno/Segno/Data/Repository/DiaryRepository.swift
+++ Segno/Segno/Data/Repository/DiaryRepository.swift
@@ -9,16 +9,16 @@ import Foundation
 import RxSwift
 
 protocol DiaryRepository {
-    func getDiaryListItem() -> Single<DiaryListItemDTO>
+    func getDiaryListItem() -> Single<DiaryListDTO>
 }
 
 final class DiaryRepositoryImpl: DiaryRepository {
-    func getDiaryListItem() -> Single<DiaryListItemDTO> {
+    func getDiaryListItem() -> Single<DiaryListDTO> {
         let endpoint = DiaryListItemEndpoint.item
         
         return NetworkManager.shared.call(endpoint)
             .map {
-                let diaryListItemDTO = try JSONDecoder().decode(DiaryListItemDTO.self, from: $0)
+                let diaryListItemDTO = try JSONDecoder().decode(DiaryListDTO.self, from: $0)
                 return diaryListItemDTO
             }
     }
