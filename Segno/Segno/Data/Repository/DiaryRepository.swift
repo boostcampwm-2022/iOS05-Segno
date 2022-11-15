ad217298aa8e63f07153cc2b38a860939f74a999
Gordon Choi <thefirate@gmail.com>
2022-11-15 15:16:40 +0900

    #28 feat: 응답을 받아 디코딩하는 메서드 작성

--- Segno/Segno/Data/Repository/DiaryRepository.swift
+++ Segno/Segno/Data/Repository/DiaryRepository.swift
@@ -5,8 +5,21 @@
 //  Created by Gordon Choi on 2022/11/15.
 //
 
+import Foundation
 import RxSwift
 
 protocol DiaryRepository {
     func getDiaryListItem() -> Single<DiaryListItemDTO>
+}
+
+final class DiaryRepositoryImpl: DiaryRepository {
+    func getDiaryListItem() -> Single<DiaryListItemDTO> {
+        let endpoint = DiaryListItemEndpoint.item
+        
+        return NetworkManager.shared.call(endpoint)
+            .map {
+                let diaryListItemDTO = try JSONDecoder().decode(DiaryListItemDTO.self, from: $0)
+                return diaryListItemDTO
+            }
+    }
 }
