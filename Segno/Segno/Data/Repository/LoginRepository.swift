31941edbfc6f467129e607f42980b1a227445d1e
Gordon Choi <thefirate@gmail.com>
2022-11-16 23:06:06 +0900

    #47 #48 feat: 현재 서버 명세대로 토큰만을 받아 반환하는 요청 함수 생성

--- Segno/Segno/Data/Repository/LoginRepository.swift
+++ Segno/Segno/Data/Repository/LoginRepository.swift
@@ -5,16 +5,22 @@
 //  Created by Gordon Choi on 2022/11/16.
 //
 
+import Foundation
 import RxSwift
 
 protocol LoginRepository {
-    func sendLoginRequest(id: String)
+    func sendLoginRequest(email: String) -> Single<TokenDTO>
     func sendLogoutRequest()
 }
 
 final class LoginRepositoryImpl: LoginRepository {
-    func sendLoginRequest(id: String) {
-        
+    func sendLoginRequest(email: String) -> Single<TokenDTO> {
+        let endpoint = LoginEndpoint.apple(email)
+        return NetworkManager.shared.call(endpoint)
+            .map {
+                let tokenDTO = try JSONDecoder().decode(TokenDTO.self, from: $0)
+                return tokenDTO
+            }
     }
     
     func sendLogoutRequest() {
