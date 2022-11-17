faadf43d521d7f78496b8eef7a0b974c7e032c47
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-17 14:55:54 +0900

    #51 #53 feat: 로그인 요청을 Repository에 보내고, 성공 여부를 ViewModel로 반환한다.

---
+++ Segno/Segno/Domain/UseCase/LoginUseCase.swift
@@ -0,0 +1,47 @@
+//
+//  LoginUseCse.swift
+//  Segno
+//
+//  Created by YOONJONG on 2022/11/17.
+//
+
+import RxSwift
+
+protocol LoginUseCase {
+    func sendLoginRequest(email: String) -> Single<Bool>
+}
+
+final class LoginUseCaseImpl: LoginUseCase {
+
+    private enum Metric {
+        static let serverToken = "serverToken"
+    }
+
+    let repository: LoginRepository
+    let localUtilityRepository: LocalUtilityRepository
+    private let disposeBag = DisposeBag()
+
+    init(repository: LoginRepository = LoginRepositoryImpl(),
+         localUtilityRepository: LocalUtilityRepository = LocalUtilityRepositoryImpl()) {
+        self.repository = repository
+        self.localUtilityRepository = localUtilityRepository
+    }
+
+    func sendLoginRequest(email: String) -> Single<Bool> {
+        return Single.create { single in
+            self.repository.sendLoginRequest(email: email)
+                .subscribe(onSuccess: { token in
+                    print("서버에서 받아온 토큰 : ", token)
+                    single(.success(true))
+                    // TODO: UserInformation Entity를 생성하여
+                    
+                    // TODO: LocalUtilityRepository에 전달한다.
+                }, onFailure: { error in
+                    single(.failure(error))
+                })
+                .disposed(by: self.disposeBag)
+
+            return Disposables.create()
+        }
+    }
+}
