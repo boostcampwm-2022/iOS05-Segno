4d6d50e97392ba7f327d5f061f4062f96a456276
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-22 16:25:37 +0900

    #71 feat: ScrollView 및 StackView로 화면 구성

---
+++ Segno/Segno/Presentation/ViewController/DiaryDetailViewController.swift
@@ -0,0 +1,118 @@
+//
+//  DiaryDetailViewController.swift
+//  Segno
+//
+//  Created by YOONJONG on 2022/11/22.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class DiaryDetailViewController: UIViewController {
+
+    let disposeBag = DisposeBag()
+//    private let viewModel: DiaryDetailViewModel
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .systemPink
+        return scrollView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .systemYellow
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private let datelabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .light)
+        label.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 30, weight: .bold)
+        label.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTemporaryData()
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        [datelabel, titleLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+            $0.width.equalTo(scrollView.snp.width)
+        }
+        
+    }
+    
+    private func setTemporaryData() {
+        datelabel.text = "11월 22일 14:54"
+        titleLabel.text = "서현에서"
+    }
+}
+
+enum DeviceType {
+    case iPhone14Pro
+
+    func name() -> String {
+        switch self {
+        case .iPhone14Pro:
+            return "iPhone 14 Pro"
+        }
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+extension UIViewController {
+
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func showPreview(_ deviceType: DeviceType = .iPhone14Pro) -> some View {
+        Preview(viewController: self).previewDevice(PreviewDevice(rawValue: deviceType.name()))
+    }
+}
+#endif
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        DiaryDetailViewController().showPreview(.iPhone14Pro)
+    }
+}
+#endif
