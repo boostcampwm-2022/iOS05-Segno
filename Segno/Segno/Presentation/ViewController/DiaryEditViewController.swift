f5b7910c1135069fe2f4af8bfce6a379419008da
Gordon Choi <thefirate@gmail.com>
2022-11-23 18:21:31 +0900

    #85 feat: 이미지 뷰, 내용 텍스트 필드, 태그 레이블을 위한 뷰 추가

--- Segno/Segno/Presentation/ViewController/DiaryEditViewController.swift
+++ Segno/Segno/Presentation/ViewController/DiaryEditViewController.swift
@@ -14,6 +14,7 @@ import SnapKit
 final class DiaryEditViewController: UIViewController {
     var diaryID: String? = nil
     
+    // TODO: 뷰로부터 적당한 간격 주기
     private lazy var mainScrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.backgroundColor = .systemPink // 테스트용 색상
@@ -24,22 +25,67 @@ final class DiaryEditViewController: UIViewController {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.backgroundColor = .systemGreen // 테스트용 색상
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
         return stackView
     }()
     
+    // TODO: 이미지 뷰를 버튼처럼 활용할 수 있도록 액션 연결
     private lazy var titleTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "제목을 입력하세요"
         textField.backgroundColor = .systemGray // 테스트용 색상
+        textField.placeholder = "제목을 입력하세요"
         return textField
     }()
     
+    private lazy var photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "popcorn")
+        imageView.backgroundColor = .systemMint // 테스트용 색상
+        return imageView
+    }()
+    
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .systemCyan // 테스트용 색상
+        return textView
+    }()
+    
+    // 태그 스택 뷰 - 태그의 관리를 어떻게 할 것인가? 
+    private lazy var tagScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .systemTeal // 테스트용 색상
+        return scrollView
+    }()
+    
+    private lazy var tagStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemBrown // 테스트용 색상
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var addTagButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black // 테스트용 색상..?
+        button.layer.cornerRadius = 8
+        button.setTitle("+", for: .normal)
+        return button
+    }()
+    
+    // 음악 검색 버튼과 음악 제목 레이블
+    // 위치 검색 버튼과 위치 레이블
+    
     init(diary: DiaryDetail?) {
+        super.init(nibName: nil, bundle: nil)
+        
         if let diary {
             diaryID = diary.id
+            titleTextField.text = diary.title
         }
-        
-        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
@@ -51,6 +97,8 @@ final class DiaryEditViewController: UIViewController {
         
         setupView()
     }
+    
+    // 내비게이션 바 세팅
     
     private func setupView() {
         view.addSubview(mainScrollView)
@@ -64,10 +112,41 @@ final class DiaryEditViewController: UIViewController {
             $0.width.equalTo(mainScrollView)
         }
         
+        setupContentsStackView()
+    }
+    
+    private func setupContentsStackView() {
         contentsStackView.addArrangedSubview(titleTextField)
         titleTextField.snp.makeConstraints {
+            $0.height.equalTo(60)
+        }
+        
+        contentsStackView.addArrangedSubview(photoImageView)
+        photoImageView.snp.makeConstraints {
+            $0.height.equalTo(400)
+        }
+        
+        contentsStackView.addArrangedSubview(bodyTextView)
+        bodyTextView.snp.makeConstraints {
+            $0.height.equalTo(400)
+        }
+        
+        contentsStackView.addArrangedSubview(tagScrollView)
+        tagScrollView.snp.makeConstraints {
             $0.height.equalTo(30)
         }
+        
+        setupTagScrollView()
+    }
+    
+    private func setupTagScrollView() {
+        tagScrollView.addSubview(tagStackView)
+        tagStackView.snp.makeConstraints {
+            $0.edges.equalTo(tagScrollView)
+            $0.height.equalTo(tagScrollView)
+        }
+        
+        
     }
 }
 
