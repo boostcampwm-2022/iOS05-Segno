2c7ea521b934b549c04e874b161435219b1cb7bc
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-24 19:42:45 +0900

    #98 feat: UI 컴포넌트 추가

--- Segno/Segno/Presentation/ViewController/MapKitViewController.swift
+++ Segno/Segno/Presentation/ViewController/MapKitViewController.swift
@@ -13,18 +13,43 @@ import SnapKit
 class MapKitViewController: UIViewController {
 
     private enum Metric {
-        static let space = 20
+        static let topSpace: CGFloat = 30
+        static let edgeSpace: CGFloat = 20
+        static let mapViewHeight: CGFloat = 500
+        static let titleSize: CGFloat = 40
+        static let buttonSize: CGFloat = 20
     }
+    
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .appFont(.surround, size: Metric.titleSize)
+        titleLabel.text = "위치"
+        return titleLabel
+    }()
+    
+    private lazy var closeButton: UIButton = {
+        let closeButton = UIButton()
+        closeButton.setImage(UIImage(systemName: "x.circle"), for: .normal)
+        closeButton.tintColor = .appColor(.color4)
+        
+        var config = UIButton.Configuration.plain()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: Metric.buttonSize)
+        config.preferredSymbolConfigurationForImage = imageConfig
+        closeButton.configuration = config
+        let action = UIAction { _ in
+            self.dismiss(animated: true)
+        }
+        closeButton.addAction(action, for: .touchUpInside)
+        return closeButton
+    }()
     
     private lazy var mapView: MKMapView = {
         let mapView = MKMapView()
-        mapView.backgroundColor = .yellow
         return mapView
     }()
     
     init(location: Location) {
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = .systemMint
         setupMapView(location: location)
     }
     
@@ -44,11 +69,23 @@ class MapKitViewController: UIViewController {
     }
     
     private func setupLayout() {
-        view.addSubview(mapView)
+        view.addSubviews([titleLabel, closeButton, mapView])
+        view.backgroundColor = .appColor(.background)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(Metric.topSpace)
+            $0.leading.equalToSuperview().inset(Metric.edgeSpace)
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(Metric.edgeSpace)
+            $0.width.height.equalTo(Metric.buttonSize)
+        }
         
         mapView.snp.makeConstraints {
-            $0.leading.trailing.top.equalTo(view).inset(20)
-            $0.height.equalTo(500)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(Metric.edgeSpace)
+            $0.leading.trailing.equalToSuperview().inset(Metric.edgeSpace)
+            $0.height.equalTo(Metric.mapViewHeight)
         }
     }
 }
