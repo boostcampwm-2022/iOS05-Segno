163f7f5670c49566faf2f12cf5c96404f64e0cfa
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-28 16:07:31 +0900

    refactor: 구글 로그인 결과를 String 타입의 Observable로 넘기도록 변경

--- Segno/Segno/Data/Session/LoginSession.swift
+++ Segno/Segno/Data/Session/LoginSession.swift
@@ -12,14 +12,12 @@ import GoogleSignIn
 import RxSwift
 
 typealias AppleLoginResult = Result<ASAuthorizationAppleIDCredential, Error>
-typealias GoogleLoginResult = Result<String, Error>
 
 final class LoginSession: NSObject {
     static let shared = LoginSession()
     
     var authorizationController: ASAuthorizationController?
     var appleCredentialResult = PublishSubject<AppleLoginResult>()
-    var googleLoginResult = PublishSubject<GoogleLoginResult>()
     
     private override init() { }
     
@@ -34,28 +32,31 @@ final class LoginSession: NSObject {
         authorizationController?.performRequests()
     }
     
-    func performGoogleLogin(_ presenter: UIViewController) {
-        let signInConfig = GIDConfiguration.init(clientID: "880830660858-2niv4cb94c63omf91uej9f23o7j15n8r.apps.googleusercontent.com")
-        
-        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: presenter) { user, error in
-            guard error == nil else { return }
-            guard let user = user else { return }
+    func performGoogleLogin(_ presenter: UIViewController) -> Observable<String> {
+        return Observable.create() { emitter in
+            let signInConfig = GIDConfiguration.init(clientID: "880830660858-2niv4cb94c63omf91uej9f23o7j15n8r.apps.googleusercontent.com")
             
-            if let userId = user.userID,                  // For client-side use only!
-               let idToken = user.authentication.idToken, // Safe to send to the server
-               let fullName = user.profile?.name,
-               let givenName = user.profile?.givenName,
-               let familyName = user.profile?.familyName,
-               let email = user.profile?.email {
-                print("Token : \(idToken)")
-                print("User ID : \(userId)")
-                print("User Email : \(email)")
-                print("User Name : \((fullName))")
-                self.googleLoginResult.onNext(.success(email))
-            } else {
-                print("Error : User Data Not Found")
-                // TODO: 커스텀 에러 타입 적용
+            GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: presenter) { user, error in
+                guard error == nil else { return }
+                guard let user = user else { return }
+                
+                if let userId = user.userID,                  // For client-side use only!
+                   let idToken = user.authentication.idToken, // Safe to send to the server
+                   let fullName = user.profile?.name,
+                   let email = user.profile?.email {
+                    debugPrint("Token : \(idToken)")
+                    debugPrint("User ID : \(userId)")
+                    debugPrint("User Email : \(email)")
+                    debugPrint("User Name : \((fullName))")
+                    
+                    emitter.onNext(email)
+                    emitter.onCompleted()
+                } else {
+                    debugPrint("Error : User Data Not Found")
+                    emitter.onError(LoginError.userDataNotFound)
+                }
             }
+            return Disposables.create()
         }
     }
 }
