88e0c8e160a48682fbfc9301bb79ac5a757bd662
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-11-28 17:51:23 +0900

    #96 feat: SettingsViewController 스위치 화면 구성

---
+++ Segno/Segno/Presentation/View/SettingsCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsCell.swift
+//  Segno
+//
+//  Created by YOONJONG on 2022/11/28.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
