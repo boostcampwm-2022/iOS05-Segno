51dc194cb1bf0dcc85adb5def4694b2639a3cbe3
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2022-12-01 00:16:28 +0900

    close #136 close #137 feat: UserDetailItem 작성

---
+++ Segno/Segno/Entity/UserDetailItem.swift
@@ -0,0 +1,12 @@
+//
+//  UserDetailItem.swift
+//  Segno
+//
+//  Created by 이예준 on 2022/11/30.
+//
+
+struct UserDetailItem: Hashable {
+    let identifier: String
+    let nickname: String
+    let writtenDiary: String
+}
