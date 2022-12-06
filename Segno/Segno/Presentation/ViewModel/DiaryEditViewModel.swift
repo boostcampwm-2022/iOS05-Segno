aaaaac7e3ce09869729f621d0cd18b898732411b
Gordon Choi <thefirate@gmail.com>
2022-12-06 14:18:57 +0900

    feat: 검색을 수행하기 위해 일기 상세 뷰 모델 정의

---
+++ Segno/Segno/Presentation/ViewModel/DiaryEditViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryEditViewModel.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2022/12/06.
+//
+
+import RxSwift
+
+final class DiaryEditViewModel {
+    var diaryDetail: DiaryDetail?
+    // 에딧 화면에 들어갈 여러 요소들
+    
+    // 일기 디테일 유즈케이스
+    
+    // 태그 추가
+    // 음악 검색 유즈케이스
+    // 위치 검색 유즈케이스
+    // 저장
+}
