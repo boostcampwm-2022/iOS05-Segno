4a59ddee5bfb21b4d18096767354162d0090eb81
Gordon Choi <thefirate@gmail.com>
2022-12-06 14:32:17 +0900

    #156 feat: 일기 작성 뷰 모델에 필요한 메서드 및 UseCase 작성

--- Segno/Segno/Presentation/ViewModel/DiaryEditViewModel.swift
+++ Segno/Segno/Presentation/ViewModel/DiaryEditViewModel.swift
@@ -11,10 +11,29 @@ final class DiaryEditViewModel {
     var diaryDetail: DiaryDetail?
     // 에딧 화면에 들어갈 여러 요소들
     
-    // 일기 디테일 유즈케이스
-    
-    // 태그 추가
-    // 음악 검색 유즈케이스
+    let diaryDetailUseCase: DiaryDetailUseCase
+    let searchMusicUseCase: SearchMusicUseCase
     // 위치 검색 유즈케이스
-    // 저장
+    
+    init(diaryDetailUseCase: DiaryDetailUseCase = DiaryDetailUseCaseImpl(),
+         searchMusicUseCase: SearchMusicUseCase = SearchMusicUseCaseImpl()) {
+        self.diaryDetailUseCase = diaryDetailUseCase
+        self.searchMusicUseCase = searchMusicUseCase
+    }
+    
+    func addTags() {
+        
+    }
+    
+    func searchMusic() {
+        
+    }
+    
+    func setLocation() {
+        
+    }
+    
+    func saveDiary() {
+        
+    }
 }
