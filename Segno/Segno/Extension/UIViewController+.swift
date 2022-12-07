bd8914a3c07b65b810148106d7f86b5a9239b009
Gordon Choi <thefirate@gmail.com>
2022-12-08 02:15:01 +0900

    feat: cancel과 ok를 지원하는 얼럿 작성

--- Segno/Segno/Extension/UIViewController+.swift
+++ Segno/Segno/Extension/UIViewController+.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 extension UIViewController {
-    func makeOKAlert(title: String, message: String, completion: @escaping () -> Void) {
+    // OK를 누르면 소멸하고 아무것도 하지 않는 얼럿입니다.
+    func makeOKAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(action)
-        self.present(alert, animated: true, completion: completion)
+        present(alert, animated: true)
     }
     
+    // 캔슬과 OK가 있는 얼럿입니다. OK에서 수행해 줄 액션을 action에 작성해주면 될 것으로 기대합니다.
+    func makeCancelOKAlert(title: String, message: String, action: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: action)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
