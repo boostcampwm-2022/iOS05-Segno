78d98de6fdf4a7f6215c0c12cbd30f17d2b791ab
Gordon Choi <thefirate@gmail.com>
2022-12-08 18:27:02 +0900

    feat: 에러 재정렬 및 MusicError 정의 후 처리

--- Segno/Segno/Data/Session/Errors/ShazamError.swift
+++ Segno/Segno/Data/Session/Errors/ShazamError.swift
@@ -9,17 +9,17 @@ import Foundation
 
 enum ShazamError: Error, LocalizedError {
     case recordDenied
-    case unknown
     case matchFailed
+    case unknown
     
     var errorDescription: String {
         switch self {
         case .recordDenied:
-            return "Record permission is denied. Please enable it in Settings."
+            return "마이크 사용 권한이 없습니다. 설정에서 마이크 사용 권한을 활성화해 주세요."
         case .matchFailed:
-            return "No song found or internet connection is bad."
+            return "노래를 찾지 못했거나, 인터넷 연결 상태가 좋지 않습니다."
         case .unknown:
-            return "Unknown error occured."
+            return "알 수 없는 오류가 발생했습니다."
         }
     }
 }
