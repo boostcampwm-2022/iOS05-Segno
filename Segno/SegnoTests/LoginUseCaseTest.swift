c7b06d354a2e4a7abc0219c4e413f2afda91d9e4
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2022-12-17 03:34:30 +0900

    feat: LoginUseCaseTest에 sendLoginRequest 테스트 추가

--- Segno/SegnoTests/LoginUseCaseTest.swift
+++ Segno/SegnoTests/LoginUseCaseTest.swift
@@ -7,23 +7,73 @@
 
 import XCTest
 
+import RxSwift
+import RxTest
+
+@testable import Segno
+
 final class LoginUseCaseTest: XCTestCase {
-    private var sut: LoginUseCase!
     
+    final class StubRepository: LoginRepository {
+        func sendLoginRequest(withApple email: String) -> Single<UserLoginDTO> {
+            return Single.create { single in
+                single(.success(UserLoginDTO(token: "token", userId: "userId")))
+                return Disposables.create()
+            }
+        }
+        
+        func sendLogoutRequest() {
+            
+        }
+    }
+    
+    var scheduler: TestScheduler!
+    var useCase: LoginUseCase!
+    var repository: LoginRepository!
+    var manager: LocalUtilityManager!
+    var info: TestableObserver<Bool>!
+    var disposeBag: DisposeBag!
+
     override func setUpWithError() throws {
         try super.setUpWithError()
         
-        sut = LoginUseCaseImpl(repository: LoginRepositoryImpl(),
-                               localUtilityManager: LocalUtilityManagerImpl())
+        scheduler = TestScheduler(initialClock: 0)
+        repository = StubRepository()
+        manager = LocalUtilityManagerImpl()
+        useCase = LoginUseCaseImpl(repository: repository,
+                                   localUtilityManager: manager)
+        info = scheduler.createObserver(Bool.self)
+        disposeBag = DisposeBag()
     }
 
     override func tearDownWithError() throws {
-        sut = nil
-        
         try super.tearDownWithError()
+        
+        scheduler = nil
+        useCase = nil
+        repository = nil
+        manager = nil
+        disposeBag = nil
     }
 
+    // MARK: 테스트를 할 때도 실제 keyChain을 바꿔버리네요..
+    // MARK: 아무리 테스트였지만.. 실행하고 나면 keyChain이 바뀌는...ㅠㅠㅠㅠ
+    // MARK: 그래서 일단 전체 주석처리 하여 실행되지 않도록 하였습니다
     func test_sendLoginRequest() throws {
-        
+//        // given
+//        let bool = true
+//        // 사실 이 값으로 테스트를 어떻게 하는게 맞지 않을까요..?
+////        let userDefaultsToken = manager.getToken(key: "userToken")
+////        let userDefaultsUserId = manager.getToken(key: "userId")
+//
+//        // when
+//        useCase.sendLoginRequest(withApple: "someToken")
+//            .asObservable()
+//            .bind(to: info)
+//            .disposed(by: disposeBag)
+//
+//        // then
+//        XCTAssertEqual(info.events, [.next(0, bool),
+//                                     .completed(0)])
     }
 }
