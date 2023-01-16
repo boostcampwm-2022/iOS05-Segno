85e0dcfdea427aeb57a9833948457e6f8d04fbcc
Gordon Choi <thefirate@gmail.com>
2023-01-16 16:21:16 +0900

    feat: 회원탈퇴 usecase 작성

---
+++ Segno/Segno/Domain/UseCase/ResignUseCase.swift
@@ -0,0 +1,32 @@
+//
+//  ResignUseCase.swift
+//  Segno
+//
+//  Created by Gordon Choi on 2023/01/16.
+//
+
+import RxSwift
+
+protocol ResignUseCase {
+    func sendResignRequest() -> Completable
+}
+
+final class ResignUseCaseImpl: ResignUseCase {
+    private enum Metric {
+        static let userToken = "userToken"
+    }
+    
+    private let repository: LoginRepository
+    private let localUtilityManager: LocalUtilityManager
+    
+    init(repository: LoginRepository = LoginRepositoryImpl(),
+         localUtilityManager: LocalUtilityManager = LocalUtilityManagerImpl()) {
+        self.repository = repository
+        self.localUtilityManager = localUtilityManager
+    }
+    
+    func sendResignRequest() -> Completable {
+        let token = localUtilityManager.getToken(key: Metric.userToken)
+        return repository.sendResignRequest(token: token)
+    }
+}
